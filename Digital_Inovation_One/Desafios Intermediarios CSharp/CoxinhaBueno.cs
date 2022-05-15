cc2f7d1b0cdbf05fc1c25e27f89efcf31bc64bd3
iagoa <iagoantunes.f@gmail.com>
2022-05-15 17:58:45 -0300

    Desafio Intermediario

---
+++ Digital_Inovation_One/Desafios Intermediarios CSharp/CoxinhaBueno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Inovation_One.Desafios_Intermediarios_CSharp
+{
+    public class CoxinhaBueno
+    {
+        static void Coxinha()
+        {
+            string[] line = Console.ReadLine().Split();
+            double X = double.Parse(line[0]);
+            double Y = double.Parse(line[1]);
+            Console.WriteLine("{0:f2}", X / Y);
+        }
+    }
+}
